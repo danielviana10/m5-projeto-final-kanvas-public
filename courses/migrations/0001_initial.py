d48204b21e14ac0d9b795ef93d78666a7722a677
danielviana10 <dani_vianaitb@outlook.com>
2023-11-06 23:35:33 -0300

    feat: finish

--- courses/migrations/0001_initial.py
+++ courses/migrations/0001_initial.py
@@ -7,7 +7,6 @@ import uuid
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -16,14 +15,41 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Course',
+            name="Course",
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('name', models.CharField(max_length=100, unique=True)),
-                ('status', models.CharField(choices=[('not started', 'Not Started'), ('in progress', 'In Progress'), ('finished', 'Finished')], default='not started', max_length=11)),
-                ('start_date', models.DateField()),
-                ('end_date', models.DateField()),
-                ('instructor', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='courses', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("name", models.CharField(max_length=100, unique=True)),
+                (
+                    "status",
+                    models.CharField(
+                        choices=[
+                            ("not started", "Not Started"),
+                            ("in progress", "In Progress"),
+                            ("finished", "Finished"),
+                        ],
+                        default="not started",
+                        max_length=11,
+                    ),
+                ),
+                ("start_date", models.DateField(auto_now_add=True)),
+                ("end_date", models.DateField()),
+                (
+                    "instructor",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="courses",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
--- courses/migrations/0002_initial.py
+++ courses/migrations/0002_initial.py
@@ -5,19 +5,22 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
-        ('courses', '0001_initial'),
+        ("courses", "0001_initial"),
+        ("students_courses", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('students_courses', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='course',
-            name='students',
-            field=models.ManyToManyField(related_name='my_courses', through='students_courses.StudentCourse', to=settings.AUTH_USER_MODEL),
+            model_name="course",
+            name="students",
+            field=models.ManyToManyField(
+                related_name="my_courses",
+                through="students_courses.StudentCourse",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
